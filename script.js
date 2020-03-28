5a020d1a7831eb09d8e956b53ff4d0759c730d15
choye921 <62723415+choye921@users.noreply.github.com>
2020-03-28 06:21:33 -0500

    Add login and review function

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+// creates a new paragraph and append it to the bottom of the HTML body.
+function createParagraph() {
+    var para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+*/
+
+  /*
+    1. Get references to all the buttons on the page and sort them in an array.
+    2. Loop through all the buttons and add a click event listener to each one.
+  
+    When any button is pressed, the createParagraph() function will be run.
+  */
+/*
+  var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+  }
+*/
+
+// new function
+
+
+function createReview() {
+    var review = document.createElement('p');
+    let name = prompt('username: ')
+    let word = prompt('Write a review');
+    review.textContent = '['+name+']   '+word;
+    document.body.appendChild(review);
+}
+
+var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', createReview);
+}
+
+
+  
+// login
+const para = document.querySelector('k');
+        para.addEventListener('click', updateName);
+        function updateName() {
+            let name = prompt('username: ');
+            para.textContent = name + '님';
+        }
+
+
